ac98ad2d04a9a45ca3efc3504bcdbe971f861dd2
mamaldo <mamaldo68@gmail.com>
2023-11-15 14:48:48 -0500

    Add mock data for testing

---
+++ src/mockData.js
@@ -0,0 +1,77 @@
+// mock data for testing
+const mockData = [
+    {
+        name: "You Know How I Do",
+        artist: "Taking Back Sunday",
+        album: "Tell All Your Friends",
+        id: "0"
+    },
+    {
+        name: "Bike Scene",
+        artist: "Taking Back Sunday",
+        album: "Tell All Your Friends",
+        id: "1"
+    },
+    {
+        name: "Cute Without the 'E' (Cut from the Team)",
+        artist: "Taking Back Sunday",
+        album: "Tell All Your Friends",
+        id: "2"
+    },
+    {
+        name: "There's No 'I' in Team",
+        artist: "Taking Back Sunday",
+        album: "Tell All Your Friends",
+        id: "3"
+    },
+    {
+        name: "Wanna Be Startin' Something",
+        artist: "Michael Jackson",
+        album: "Thriller",
+        id: "4"
+    },
+    {
+        name: "Baby Be Mine",
+        artist: "Michael Jackson",
+        album: "Thriller",
+        id: "5"
+    },
+    {
+        name: "The Girl Is Mine",
+        artist: "Michael Jackson",
+        album: "Thriller",
+        id: "6"
+    },
+    {
+        name: "Thriller",
+        artist: "Michael Jackson",
+        album: "Thriller",
+        id: "7"
+    },
+    {
+        name: "Jenny Was a Friend of Mine",
+        artist: "The Killers",
+        album: "Hot Fuss",
+        id: "8"
+    },
+    {
+        name: "Mr. Brightside",
+        artist: "The Killers",
+        album: "Hot Fuss",
+        id: "9"
+    },
+    {
+        name: "Smile Like You Mean It",
+        artist: "The Killers",
+        album: "Hot Fuss",
+        id: "10"
+    },
+    {
+        name: "Somebody Told Me",
+        artist: "The Killers",
+        album: "Hot Fuss",
+        id: "11"
+    }
+];
+
+export default mockData;
