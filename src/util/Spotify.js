065276a73ead178f6a9fcc7e7559e2291717a2b4
mamaldo <mamaldo68@gmail.com>
2023-11-19 14:37:18 -0500

    Add ability for get access to the access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,39 @@
 const clientID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const redirectURI = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+let accessToken ="";
 
 const Spotify = {
     getAccess () {
-        let url = "https://accounts.spotify.com/authorize";
-        url += "?response_type=token";
-        url += "&client_id=" + encodeURIComponent(clientID);
-        url += "&redirect_uri=" + encodeURIComponent(redirectURI);
+        if(accessToken) {
+            return accessToken;
+        }
 
-        window.location = url;
+        const queryParams = window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            if(item) {
+                const parts = item.split("=");
+                initial[parts[0]] = decodeURIComponent(parts[1]);
+            }
+            return initial;
+        }, {});
+        
+        accessToken = queryParams["access_token"];
+        const expiresIn = queryParams["expires_in"];
+
+        if(accessToken && expiresIn) {
+            setTimeout(() => accessToken = "", expiresIn * 1000); // clears out expired access token
+            window.location.hash = ""; // clears parameters from URL
+            return accessToken;
+        } else {
+            let url = "https://accounts.spotify.com/authorize";
+            url += "?response_type=token";
+            url += "&client_id=" + encodeURIComponent(clientID);
+            url += "&redirect_uri=" + encodeURIComponent(redirectURI);
+
+            window.location = url;
+        }
     }
 };
 
